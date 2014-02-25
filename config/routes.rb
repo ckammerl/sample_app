de8ae2205d91df0d16b56bbe5edb131cc880eb98
Christiane Kammerl <christiane@kammerl.com>
2014-02-24 16:39:26 -0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,28 @@
+# This arranges both for a valid page at /help (responding to GET requests) and a named route called help_path that 
+# returns the path to that page. (Actually, using 'get' in place of 'match' gives the same named routes, but using match 
+# is more conventional.)
+
+# the code match ’/about’ automatically creates named routes for use in the controllers and views: 
+    # about_path -> /about
+    # about_url -> http://localhost:3000/about
+
+# You can have the root of your site routed with "root"
+  # root 'welcome#index' or in this case root 'static_pages#home' and we also automatically get the URL helpers (named routes):
+      #root_path -> '/'
+      #root_url  -> 'http://localhost:3000/'
+
+
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  get "users/new"
+
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get' # As with the other routes, match ’/signup’ gives us the named route signup_path
+  match '/help',    to: 'static_pages#help',    via: 'get' 
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,43 +1,64 @@
+#RSpec uses the general malleability of Ruby to define a domain-specific language (DSL) built just for testing:
+
+
 require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" } 
+  # eliminating some of the duplication by defining a 'base_title' variable and using string interpolation below
+  # Here only used for Sample App page, ie the 'root' test
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+   subject { page } 
+   # the individual 'page variable' (Help, About, Contact) is the 'subject' of the test; 
+   # NOT used for Sample App, ie 'root' test
+
+
+
+# RSpec: The first line indicates that we are describing the Home page. 
+# This description is just a string, and it can be anything you want; 
+# Then the spec says that when you visit the Home page at 'root_path', the content should contain the words “Sample App”. 
+# As with the first line, what goes inside the quote marks is IRRELEVANT to RSpec, and is intended to be descriptive to human readers. 
+  describe "Home page" do 
+    before { visit root_path }  # use the 'before' method to visit the root_path BEFORE EACH example, ie: before each 'it' method:
+
+    it "should have the h1 'Sample App'" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    it "should have the base title" do
+      expect(page).to have_title("#{base_title}")
+    end
+  
+   it "should not have a custom page title" do
+      expect(page).not_to have_title('| Home')
     end
   end
+
+
 
   describe "Help page" do
+    before { visit help_path } # use the 'before' method to visit the help_path BEFORE EACH example, ie: before each 'it' method:
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
+    it { should have_content('Help') } # 'it' method shortened: Because of 'subject { page }', the call to 'should' automatically uses the page variable supplied by Capybara
+    it { should have_title(full_title('Help')) } # see spec/support/utilities.rb for 'full_title' method
   end
+
+
 
   describe "About page" do
+    before { visit about_path } 
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
-end
+
+
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+  	it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
+end 
